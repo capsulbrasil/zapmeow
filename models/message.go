0be313835003239e609b8ca718b71d755df472e9
Gabriel Augusto <gabrielolivrp@gmail.com>
2023-09-05 17:08:01 -0300

    fix: unique index

--- models/message.go
+++ models/message.go
@@ -11,7 +11,7 @@ type Message struct {
 	SenderJID  string `gorm:"column:sender_jid"`
 	ChatJID    string `gorm:"column:chat_jid"`
 	InstanceID string
-	MessageID  string `form:"unique"`
+	MessageID  string `gorm:"uniqueIndex"`
 	Timestamp  time.Time
 	Body       string
 	MediaType  string
